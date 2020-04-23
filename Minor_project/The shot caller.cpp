294bdd0ca258c3778b79de7ebc91c65665b3d3d9
gitynity <nitybriliantluckybro@gmail.com>
2020-04-23 22:25:19 +0530

    Update The shot caller.cpp

--- Minor_project/The shot caller.cpp
+++ Minor_project/The shot caller.cpp
@@ -1,28 +1,14 @@
-#include<fstream>
+#include<iostream>
 #include<string>
 #include<unistd.h>
 using namespace std;
 
 int main()
 {
-		string data;
-	
-		ifstream input ("system_crasher.cpp");
-		ofstream output ("system_crasher2.cpp");
-		while(!input.eof())
-		{
-			
-			getline(input , data);
-			output<<data;
-			output<<"\n";
-			
-		}
-		input.close();
-		output.close();
 		
 		//string cwd;
 		//cwd = get_current_dir_name();
 		system("cd $pwd");
-		system("g++ system_crasher2.cpp");
+		system("g++ system_crasher.cpp");
 		system("./a.out");
 }
